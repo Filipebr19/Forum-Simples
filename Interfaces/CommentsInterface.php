01057dafde5154c47a0e81c2335d1954dc109a7b
Filipe Loredo <95660776+Filipebr19@users.noreply.github.com>
2024-04-24 09:44:15 -0300

    Refazendo projeto

--- Interfaces/CommentsInterface.php
+++
@@ -1,6 +0,0 @@
-<?php 
-namespace Interfaces\CommentsInterface;
-
-abstract class CommentsInterface {
-    
-}
--- pages/sair.php
+++
@@ -1,6 +0,0 @@
-<?php 
-session_start();
-
-$_SESSION['id'] = null;
-header('Location: ../index.php');
-exit;
