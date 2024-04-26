92ff4f1ecbbe6db6b3451e456bf11b3fe1022465
Filipe Loredo <95660776+Filipebr19@users.noreply.github.com>
2024-04-26 11:02:38 -0300

    Db Interface finalizado

--- Interfaces/DbInterface.php
+++ Interfaces/DbInterface.php
@@ -4,9 +4,18 @@ namespace Interfaces;
 interface DbInterface {
     public function createUser(UserInterface $user);
     public function createPost(PostInterface $post);
-    public function readId(int $id);
+    public function createComment(CommentInterface $comment);
+
+    public function readUserId(int $id);
     public function readEmail(string $email);
     public function readPostById(int $idPost);
+    public function readCommentById(int $idComment);
+
     public function updateUser(UserInterface $user);
+    public function updatePost(PostInterface $post);
+    public function updateComment(CommentInterface $comment);
+
     public function deleteUser(int $id);
+    public function deletePost(int $idPost);
+    public function deleteComment(int $idComment);
 };
